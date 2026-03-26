a25c43133058d788878073c8a0057d36e4e46d47
Debjeet Dhar <148609494+Debjeet-Dhar@users.noreply.github.com>
2026-03-26 15:56:30 +0530

    Enhance product image selection and quantity controls
    Added increment and decrement functionality for quantity.

--- 01_ecommerce-product-page/js/js.js
+++ 01_ecommerce-product-page/js/js.js
@@ -1,10 +1,25 @@
 const thumbnail = document.getElementsByClassName("placeholder")
 const sample = document.querySelectorAll('.image-holder')
-// console.dir(sample);
+const incress = document.querySelector('.incress')
+const decress = document.querySelector('.decress')
+const value = document.querySelector('.value')
+
 
 
 sample.forEach((s)=>{
   s.addEventListener("click",()=>{
-    thumbnail[0].childNodes[1].src = s.children[0].src
+   const originalUrl = s.children[0].src
+   const fullUrl = originalUrl.replace('-thumbnail', '');
+   thumbnail[0].childNodes[1].src = fullUrl
   })
+})
+
+incress.addEventListener("click",()=>{
+    value.textContent ++
+})
+
+decress.addEventListener("click",()=>{
+  if(value.textContent > 0) {
+    value.textContent --
+  }
 })
